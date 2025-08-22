ae639ea5291de519eabb00cb2593495dea0e278a
rodrigoprogramacao <rodrigofloresdacosta97@gmail.com>
2025-08-22 03:05:14 +0000

    NovoCommit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
-
 int main() {
-printf("Desafio Cartas Super Trunfo!\n");
+char estadoA, estadoB;
+char codigo[5];
+char cidadeA[50], cidadeB[50];
+int pontosA;
+float areaA, pibA, populacaoA;
+
+printf("CADASTRO CARTA 01 - SUPER TRUNFO!\n");
+
+printf("Digite a letra correspondente ao seu Estado:\n");
+scanf(" %c", &estadoA);
+
+printf("Digite sua cidade:\n");
+scanf("%s", &cidadeA);
+
+printf("Digite seu código:\n");
+scanf("%s", &codigo);
+
+printf("Digite a população:\n");
+scanf("%f", &populacaoA);
+
+printf("Digite a área em km²:\n");
+scanf("%f", &areaA);
+
+printf("Digite o PIB:\n");
+scanf(" %f, &pibA");
+
+printf("Digite o número de pontos turísticos:\n");
+scanf(" %d, &pontosA");
+
+
+
+
 
     return 0;
 }
