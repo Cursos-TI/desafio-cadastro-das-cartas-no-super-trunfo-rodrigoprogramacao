5cb74926468a4320fe6174a3b171a5254c68fbe1
rodrigoprogramacao <rodrigofloresdacosta97@gmail.com>
2025-08-22 03:22:48 +0000

    NOVO_COMMIT

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main() {
 char estadoA, estadoB;
-char codigo[5];
+char codigoA[5], codigoB[5];
 char cidadeA[50], cidadeB[50];
 int pontosA;
 float areaA, pibA, populacaoA;
@@ -16,20 +16,33 @@ printf("Digite sua cidade:\n");
 scanf("%s", &cidadeA);
 
 printf("Digite seu código:\n");
-scanf("%s", &codigo);
+scanf("%s", &codigoA);
 
 printf("Digite a população:\n");
-scanf("%f", &populacaoA);
+scanf("%d", &populacaoA);
 
 printf("Digite a área em km²:\n");
 scanf("%f", &areaA);
 
 printf("Digite o PIB:\n");
-scanf(" %f, &pibA");
+scanf("%f, &pibA");
 
 printf("Digite o número de pontos turísticos:\n");
-scanf(" %d, &pontosA");
+scanf("%d, &pontosA");
 
+printf("\n");
+
+printf("CARTA 01 CADASTRADA COM SUCESSO!\n");
+
+printf("\n");
+
+printf("Estado: %c \n", estadoA);
+printf("Cidade: %s \n", cidadeA);
+printf("Código: %s \n", codigoA);
+printf("População: %d \n", populacaoA);
+printf("Área em km²: %f \n", areaA);
+printf("PIB: %f \n", pibA);
+printf("Número de pontos turísticos: %d \n", pontosA);
 
 
 
